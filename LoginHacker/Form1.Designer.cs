c567b12944f8dba972db42414226127d47008ce3
Blue <zzx094@gmail.com>
2015-12-12 14:08:19 +0800

    1st commit

---
+++ LoginHacker/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LoginHacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label3 = new System.Windows.Forms.Label();
+            this.DIC = new System.Windows.Forms.TextBox();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.LOG = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.KEY = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.URL = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FLAG = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(17, 80);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(25, 13);
+            this.Label3.TabIndex = 15;
+            this.Label3.Text = "DIC";
+            // 
+            // DIC
+            // 
+            this.DIC.Location = new System.Drawing.Point(51, 77);
+            this.DIC.Name = "DIC";
+            this.DIC.Size = new System.Drawing.Size(215, 20);
+            this.DIC.TabIndex = 14;
+            this.DIC.TextChanged += new System.EventHandler(this.DIC_TextChanged);
+            this.DIC.DoubleClick += new System.EventHandler(this.TextBox4_DoubleClick);
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(197, 223);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 13;
+            this.Button1.Text = "Start";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // LOG
+            // 
+            this.LOG.Location = new System.Drawing.Point(24, 131);
+            this.LOG.Multiline = true;
+            this.LOG.Name = "LOG";
+            this.LOG.Size = new System.Drawing.Size(248, 86);
+            this.LOG.TabIndex = 12;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(16, 54);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(28, 13);
+            this.Label2.TabIndex = 11;
+            this.Label2.Text = "KEY";
+            // 
+            // KEY
+            // 
+            this.KEY.Location = new System.Drawing.Point(51, 51);
+            this.KEY.Name = "KEY";
+            this.KEY.Size = new System.Drawing.Size(215, 20);
+            this.KEY.TabIndex = 10;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(16, 28);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(29, 13);
+            this.Label1.TabIndex = 9;
+            this.Label1.Text = "URL";
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(51, 25);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(215, 20);
+            this.URL.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "FLAG";
+            // 
+            // FLAG
+            // 
+            this.FLAG.Location = new System.Drawing.Point(51, 103);
+            this.FLAG.Name = "FLAG";
+            this.FLAG.Size = new System.Drawing.Size(215, 20);
+            this.FLAG.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FLAG);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.DIC);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.LOG);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.KEY);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.URL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginHacker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox DIC;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.TextBox LOG;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox KEY;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        internal System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.TextBox FLAG;
+    }
+}
+
---
+++ LoginHacker/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginHacker
+{
+    public partial class Form1 : Form
+    {
+
+        private string PostData(string url, string postString)
+        {
+            string srcString="";
+            try 
+            {
+                byte[] postData = Encoding.UTF8.GetBytes(postString);
+                WebClient webClient = new WebClient();
+                webClient.UseDefaultCredentials = true;
+                webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                byte[] responseData = webClient.UploadData(url, "POST", postData);
+                srcString = Encoding.UTF8.GetString(responseData);
+                
+            }
+            catch { }
+            return srcString;  
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox4_DoubleClick(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                string file = openFileDialog1.FileName;
+                DIC.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void DIC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try 
+            {
+                //RegistryKey key = Microsoft.Win32.Registry.LocalMachine;
+                //RegistryKey Config = key.OpenSubKey("software//LoginHacker");
+                //URL.Text = Config.GetValue("URL").ToString();
+                //KEY.Text = Config.GetValue("KEY").ToString();
+                //FLAG.Text = Config.GetValue("FLAG").ToString();
+            }
+            catch { }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //保存配置
+            //RegistryKey key = Microsoft.Win32.Registry.LocalMachine;
+            //RegistryKey Config = key.CreateSubKey("software//LoginHacker");
+            //Config.SetValue("URL", URL.Text);
+            //Config.SetValue("KEY", KEY.Text);
+            //Config.SetValue("FLAG", FLAG.Text);
+            //
+            Button1.Enabled = false;
+            StreamReader sr = new StreamReader(DIC.Text);
+            
+            while (!sr.EndOfStream)
+            {
+                Application.DoEvents();
+                string DicStr = sr.ReadLine();
+                string KeyStr = KEY.Text.Replace("{DIC}", DicStr);
+                string ResStr = PostData(URL.Text, KeyStr);
+                LOG.Text = "TRY>" + DicStr;
+                if (ResStr.IndexOf(FLAG.Text) > -1)
+                {
+                    LOG.Text = "KEY>" + DicStr;
+                    break;
+                }
+            }
+            Button1.Enabled = true;
+        }
+
+
+    }
+}
